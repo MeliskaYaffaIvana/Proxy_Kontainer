23976044ad100bfa1abc09c66c6771625b29e0e9
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 13:29:38 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -9,30 +9,32 @@ if response.status_code == 200:
     # Mendapatkan data JSON dari respons
     data = response.json()
 
-    # Memproses data JSON
-    for category, containers in data.items():
-        for container in containers:
-            id = container['id']
-            nim = container['nim']
-            port = container['port']
+    # Membuat file untuk menyimpan semua konfigurasi
+    nama_file = "config.txt"
 
-            acl_line = f"acl svr_{id} hdr(host) -i {nim}.jti.polinema.ac.id"
-            use_backend_line = f"use_{category} be_{id} if svr_{id}"
-            backend_block = f"""
+    # Menulis ke file menggunakan open()
+    with open(nama_file, 'w') as file:
+        # Memproses data JSON
+        for category, containers in data.items():
+            for container in containers:
+                id = container['id']
+                nim = container['nim']
+                port = container['port']
+
+                acl_line = f"acl svr_{id} hdr(host) -i {nim}.jti.polinema.ac.id"
+                use_backend_line = f"use_{category} be_{id} if svr_{id}"
+                backend_block = f"""
 {category} be_{id}
     mode http
     option forwardfor
     server 10.0.0.21 10.0.0.21:{port}
 """
 
-            # Membuat file dengan nama kategori dan id sebagai nama file
-            nama_file = f"{category}_{id}.txt"
-
-            # Menulis ke file menggunakan open()
-            with open(nama_file, 'w') as file:
+                # Menulis konfigurasi ke file
                 file.write(acl_line + "\n")
                 file.write(use_backend_line + "\n")
                 file.write(backend_block)
+                file.write("\n")  # Menambahkan baris kosong sebagai pemisah antara konfigurasi
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
