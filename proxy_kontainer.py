5b1f4f04e0e622ac6240bee5546e550c5a037993
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 12:37:33 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -1,5 +1,4 @@
 import requests
-import subprocess
 
 # Membuat permintaan GET ke API Laravel untuk mendapatkan data kontainer per kategori
 url = 'http://10.0.0.19/api/containers'  # Ganti URL dengan URL API Laravel yang sesuai
@@ -9,19 +8,19 @@ response = requests.get(url)
 if response.status_code == 200:
     # Mendapatkan data JSON dari respons
     data = response.json()
-    print(data)
 
     # Memproses data JSON
     for category, containers in data.items():
         print(f"Kategori: {category}")
+        
 
-        # Membuat file dengan nama kategori sebagai nama file
-        nama_file = f"{category}.txt"
-        isi_teks = f"tetaplah mencoba"
+            # Membuat file dengan nama kategori sebagai nama file
+            nama_file = f"{category}.txt"
+            isi_teks = f"Nama Kontainer: {container['nama_kontainer']}\nNIM: {container['nim']}"
 
-        # Menjalankan perintah echo melalui shell untuk menulis ke file
-        subprocess.run(['echo', isi_teks, '>', nama_file], shell=True)
-
+            # Menulis ke file menggunakan open()
+            with open(nama_file, 'w') as file:
+                file.write(isi_teks)
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
