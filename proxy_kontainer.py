4c2228d3f95888ef5cb556271f606c96ac0e998c
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 13:10:43 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -11,23 +11,28 @@ if response.status_code == 200:
 
     # Memproses data JSON
     for category, containers in data.items():
-        acl_line = f"acl svr_{category} hdr(host) -i 1941720020.jti.polinema.ac.id"
-        use_backend_line = f"use_backend be_{category} if svr_{category}"
-        backend_block = f"""
-backend be_{category}
+        for container in containers:
+            id = container['id']
+            nim = container['nim']
+            port_kontainer = container['port_kontainer']
+
+            acl_line = f"acl svr_{category}_{id} hdr(host) -i 1941720020.jti.polinema.ac.id"
+            use_backend_line = f"use_backend be_{category}_{id} if svr_{category}_{id}"
+            backend_block = f"""
+backend be_{category}_{id}
     mode http
     option forwardfor
-    server 10.0.0.21 10.0.0.21:10001
+    server 10.0.0.21 10.0.0.21:{port_kontainer}
 """
 
-        # Membuat file dengan nama kategori sebagai nama file
-        nama_file = f"{category}.txt"
+            # Membuat file dengan nama kategori dan id sebagai nama file
+            nama_file = f"{category}_{id}.txt"
 
-        # Menulis ke file menggunakan open()
-        with open(nama_file, 'w') as file:
-            file.write(acl_line + "\n")
-            file.write(use_backend_line + "\n")
-            file.write(backend_block)
+            # Menulis ke file menggunakan open()
+            with open(nama_file, 'w') as file:
+                file.write(acl_line + "\n")
+                file.write(use_backend_line + "\n")
+                file.write(backend_block)
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
