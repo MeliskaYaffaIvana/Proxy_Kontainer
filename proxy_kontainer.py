6376f5c5a5b328c0c88379ea4a0dcfc41b48792b
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 12:31:43 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -14,25 +14,13 @@ if response.status_code == 200:
     # Memproses data JSON
     for category, containers in data.items():
         print(f"Kategori: {category}")
-        # for container in containers:
-        #     if 'nama_kontainer' in container:
-        #         print(f"Nama Kontainer: {container['nama_kontainer']}")
-        #     else:
-        #         print("Kunci 'nama_kontainer' tidak ditemukan dalam objek kontainer")
-        #     # Lakukan operasi lain sesuai kebutuhan
-
-        #     if 'nim' in container:
-        #         print(f"NIM: {container['nim']}")
-        #     else:
-        #         print("Kunci 'nim' tidak ditemukan dalam objek kontainer")
-        
 
         # Membuat file dengan nama kategori sebagai nama file
-            nama_file = f"{category}.txt"
-            isi_teks = f"Nama Kontainer: {container['nama_kontainer']}\nNIM: {container['nim']}"
+        nama_file = f"{category}.txt"
+        isi_teks = f"Nama Kontainer: {container['nama_kontainer']}\nNIM: {container['nim']}"
 
-            # Menjalankan perintah echo melalui shell untuk menulis ke file
-            subprocess.run(['echo', isi_teks, '>', nama_file], shell=True)
+        # Menjalankan perintah echo melalui shell untuk menulis ke file
+        subprocess.run(['echo', isi_teks, '>', nama_file], shell=True)
 
 
 else:
