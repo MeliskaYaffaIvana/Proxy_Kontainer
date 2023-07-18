4a84a2287fe5d77500680662578dee7eeac0dca6
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 16:17:30 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -91,7 +91,7 @@ backend be_{id}
 
         # Menulis semua use_backend_line ke file
         for use_backend_line in use_backend_lines:
-            file.write(use_backend_line )
+            file.write(use_backend_line + "\n")
 
         # Menulis semua backend_block ke file
         for backend_block in backend_blocks:
