67ddabc51637b7eaa3e52a5cf8e4a4e46ef26699
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 15:17:48 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -12,24 +12,34 @@ if response.status_code == 200:
     # Membuat file untuk menyimpan semua konfigurasi
     nama_file = "proxy.txt"
 
-    # Menulis ke file menggunakan open()
-    with open(nama_file, 'w') as file:
-        # Memproses data JSON
-        for category, containers in data.items():
-            for container in containers:
-                id = container['id']
-                nim = container['nim']
-                port = container['port']
+    # Menyiapkan variabel untuk menyimpan data acl_line, use_backend_line, dan backend_block
+    acl_lines = []
+    use_backend_lines = []
+    backend_blocks = []
 
-                acl_line = f"    acl svr_{id} hdr(host) -i {nim}.jti.polinema.ac.id"
-                use_backend_line = f"    use_{category} be_{id} if svr_{id}"
-                backend_block = f"""
+    # Memproses data JSON
+    for category, containers in data.items():
+        for container in containers:
+            id = container['id']
+            nim = container['nim']
+            port = container['port']
+
+            acl_line = f"    acl svr_{id} hdr(host) -i {nim}.jti.polinema.ac.id"
+            use_backend_line = f"    use_{category} be_{id} if svr_{id}"
+            backend_block = f"""
 {category} be_{id}
     mode http
     option forwardfor
     server 10.0.0.21 10.0.0.21:{port}
 """
-                
+
+            # Menyimpan data ke variabel
+            acl_lines.append(acl_line)
+            use_backend_lines.append(use_backend_line)
+            backend_blocks.append(backend_block)
+
+    # Menulis ke file menggunakan open()
+    with open(nama_file, 'w') as file:
         # Menulis semua acl_line ke file
         for acl_line in acl_lines:
             file.write(acl_line + "\n")
@@ -41,7 +51,6 @@ if response.status_code == 200:
         # Menulis semua backend_block ke file
         for backend_block in backend_blocks:
             file.write(backend_block + "\n")
-
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
