a28864467910cec1b1eb4cd0a96003596f6b3157
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 13:06:50 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -11,15 +11,23 @@ if response.status_code == 200:
 
     # Memproses data JSON
     for category, containers in data.items():
-        print(f"Kategori: {category}")
-        for container in containers:
-            # Membuat file dengan nama kategori sebagai nama file
-            nama_file = f"{category}.txt"
-            isi_teks = f"Nama Kontainer: {container['id']}\nNIM: {container['nim']}"
+        acl_line = f"acl svr_{category} hdr(host) -i 1941720020.jti.polinema.ac.id"
+        use_backend_line = f"use_backend be_{category} if svr_{category}"
+        backend_block = f"""
+backend be_{category}
+    mode http
+    option forwardfor
+    server 10.0.0.21 10.0.0.21:10001
+"""
 
-            # Menulis ke file menggunakan open()
-            with open(nama_file, 'w') as file:
-                file.write(isi_teks)
+        # Membuat file dengan nama kategori sebagai nama file
+        nama_file = f"{category}.txt"
+
+        # Menulis ke file menggunakan open()
+        with open(nama_file, 'w') as file:
+            file.write(acl_line + "\n")
+            file.write(use_backend_line + "\n")
+            file.write(backend_block)
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
