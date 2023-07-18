1ea80581ff07951e451bf5d666088811dcea7fa3
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 12:45:47 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -12,8 +12,6 @@ if response.status_code == 200:
     # Memproses data JSON
     for category, containers in data.items():
         print(f"Kategori: {category}")
-        
-
             # Membuat file dengan nama kategori sebagai nama file
             nama_file = f"try.txt"
             isi_teks = f"Nama Kontainer: {container['nama_kontainer']}\nNIM: {container['nim']}"
@@ -21,6 +19,9 @@ if response.status_code == 200:
             # Menulis ke file menggunakan open()
             with open(nama_file, 'w') as file:
                 file.write(isi_teks)
+        
+
+            
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
