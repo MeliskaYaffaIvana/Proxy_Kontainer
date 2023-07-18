500d3e10bff86d2c54ac7d5902e7e75aff5fbdb5
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-18 12:11:05 +0700

    Update proxy_kontainer.py

--- proxy_kontainer.py
+++ proxy_kontainer.py
@@ -13,17 +13,17 @@ if response.status_code == 200:
     # Memproses data JSON
     for category, containers in data.items():
         print(f"Kategori: {category}")
-        for container in containers:
-            if 'nama_kontainer' in container:
-                print(f"Nama Kontainer: {container['nama_kontainer']}")
-            else:
-                print("Kunci 'nama_kontainer' tidak ditemukan dalam objek kontainer")
-            # Lakukan operasi lain sesuai kebutuhan
+        # for container in containers:
+        #     if 'nama_kontainer' in container:
+        #         print(f"Nama Kontainer: {container['nama_kontainer']}")
+        #     else:
+        #         print("Kunci 'nama_kontainer' tidak ditemukan dalam objek kontainer")
+        #     # Lakukan operasi lain sesuai kebutuhan
 
-            if 'nim' in container:
-                print(f"NIM: {container['nim']}")
-            else:
-                print("Kunci 'nim' tidak ditemukan dalam objek kontainer")
+        #     if 'nim' in container:
+        #         print(f"NIM: {container['nim']}")
+        #     else:
+        #         print("Kunci 'nim' tidak ditemukan dalam objek kontainer")
 
 else:
     print(f"Permintaan gagal dengan kode status {response.status_code}")
